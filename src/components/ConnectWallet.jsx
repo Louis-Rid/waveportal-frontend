28414e054e69498202c06a5b82a9392f4eaaa33c
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-05-23 09:42:03 -0600

    Added styles

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,27 +1,27 @@
-const ConnectWallet = ({ethereum, setCurrentAccount}) => {
-    const connectWallet = async () => {
-        try {
-            if(!ethereum) {
-                alert("You need to install metamask on your browser.")
-                return;
-            }
+const ConnectWallet = ({ ethereum, setCurrentAccount, styles }) => {
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        alert("You need to install metamask on your browser.");
+        return;
+      }
 
-            const accounts = await ethereum.request({
-                method: "eth_requestAccounts",
-              });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-            console.log(`Connected ${accounts[0]}`)
-            setCurrentAccount(accounts[0]);
-        } catch (e) {
-            console.log(e);
-        }
+      console.log(`Connected ${accounts[0]}`);
+      setCurrentAccount(accounts[0]);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    return (
-        <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-        </button>
-    )
-}
+  return (
+    <button className={styles} onClick={connectWallet}>
+      Connect Wallet
+    </button>
+  );
+};
 
 export default ConnectWallet;
