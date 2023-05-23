772ea7ac005b6c9f2016f20413a1edef401ad39a
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-05-23 09:38:57 -0600

    Removed testing conosle.logs and added styles

--- src/components/Wave.jsx
+++ src/components/Wave.jsx
@@ -1,42 +1,47 @@
 import { ethers } from "ethers";
-import abi from "../utils/WavePortal.json";
 
-const Wave = ({contractAddress}) => {
-    const wave = async () => {
-        try {
-          const { ethereum } = window;
-      
-          if (ethereum) {
-            const provider = new ethers.providers.Web3Provider(ethereum)
-            const signer = provider.getSigner();
-            const contractABI = abi.abi;
-            const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-            
-            // Check initial wave count
-            let count = await wavePortalContract.getTotalWaves();
-            console.log(`Retrieved toal wave count of ${count.toNumber()}`)
+const Wave = ({ contractAddress, abi, message, styles }) => {
+  const wave = async () => {
+    console.log(abi);
+    try {
+      const { ethereum } = window;
 
-            // Execute wave() function from smart contract
-            const waveTxn = await wavePortalContract.wave();
-            console.log(`Mining -- ${waveTxn.hash}`);
-            await waveTxn.wait(1);
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          abi,
+          signer
+        );
 
-            // Check wave count after completting a wave
-            count = await wavePortalContract.getTotalWaves();
-            console.log(`Retrieved toal wave count of ${count.toNumber()}`)
-          } else {
-            console.log(`Ethereum object doesn't exist.`)
-          }
-        } catch (e) {
-          console.log(e);
-        }
+        // Check initial wave count
+        let count = await wavePortalContract.getTotalWaves();
+        console.log(`Retrieved toal wave count of ${count.toNumber()}`);
+
+        // Execute wave() function from smart contract
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 300000,
+        });
+        console.log(`Mining -- ${waveTxn.hash}`);
+        await waveTxn.wait(1);
+
+        // Check wave count after completting a wave
+        count = await wavePortalContract.getTotalWaves();
+        console.log(`Retrieved toal wave count of ${count.toNumber()}`);
+      } else {
+        console.log(`Ethereum object doesn't exist.`);
       }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
-        <button onClick={wave}>
-            Wave!
-        </button>
-    )
-}
+  return (
+    <button className={styles} onClick={wave}>
+      Submit
+    </button>
+  );
+};
 
 export default Wave;
